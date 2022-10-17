91b608d3159bc255c20fb3bc224245b1fe21b0e9
ush <norbuurgen@gmail.com>
2022-10-17 15:26:50 +0545

    create struct

--- AsyncLog.go
+++ AsyncLog.go
@@ -1,1 +1,16 @@
 package AsyncLogging
+
+import (
+	"io"
+	"sync"
+)
+
+// Basic logger struct. Use message channel for async logging and writer for synchronous logging
+type TLog struct {
+	dest             io.Writer
+	m                *sync.Mutex
+	msgCh            chan string
+	errCh            chan error
+	shutdownCh       chan struct{}
+	shutdownComplete chan struct{}
+}
